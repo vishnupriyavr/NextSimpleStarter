6138ee2ec0048e538cea8dc442dcc05bbcf8bbe5
vishnupriyavr <papudear@gmail.com>
2020-11-24 14:20:45 +0000

    Templates Initial Commit

---
+++ pages/templates.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import AppMenuBar from "../components/AppMenuBar"
+import ViewingMode from "../components/ViewingMode"
+//import MediaControlCard from "../components/VideoEmbed"
+
+
+function Templates() {
+	return(
+		<div>
+			<ViewingMode>
+				<AppMenuBar />
+			</ViewingMode>
+		</div>
+
+
+	);
+}
+
+export default Templates
