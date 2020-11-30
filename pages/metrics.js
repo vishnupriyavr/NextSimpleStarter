aa6fb041803e21f83077bed5eb6d4023c55b7da2
vishnupriyavr <papudear@gmail.com>
2020-11-30 05:42:22 +0000

    Dashboard Changes

--- pages/metrics.js
+++ pages/metrics.js
@@ -1,17 +1,38 @@
 import React from 'react';
-import ViewingMode from "../components/ViewingMode"
 import Dashboard from "../components/Dashboard"
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 export default function Templates() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+				typography: {
+    fontFamily: ['"Lato"', 'sans-serif'].join(','),
+	},
+
+        palette: {
+    			primary: {
+      		light: '#757ce8',
+      		main: '#3f50b5',
+      		dark: '#002884',
+      		contrastText: '#fff',
+   				 },
+          type: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+	);
+
 	return(
-		<div>
 
-			<ViewingMode/>
+			<ThemeProvider theme={theme}>
+      <CssBaseline/>
 				<Dashboard />
-
-
-
-		</div>
+    </ThemeProvider>
 
 
 	);
